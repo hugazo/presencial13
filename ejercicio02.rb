281558dfaf81e88c0837c98146ce203a1f26eb2e
Hugo Morales <hmoralesg@icloud.com>
2017-06-20 19:30:31 -0400

    Ejercicio 02 Finalizado

--- ejercicio02.rb
+++ ejercicio02.rb
@@ -1,2 +1,16 @@
-productos = {'bebida' => 850, 'chocolate' => 1200, 'galletas' => 900, 'leche' => 750}
-productos.each { |key, value| puts "#{key} : #{value}" }
+productos = {
+  'bebida' => 850,
+  'chocolate' => 1200,
+  'galletas' => 900,
+  'leche' => 750
+}
+def listar_productos(productos)
+  productos.each { |key, value| puts "#{key} : #{value}" }
+end
+productos['cereal'] = 2200
+productos['bebida'] = 2000
+listar_productos(productos)
+productos_array = productos.to_a
+puts productos_array
+productos.delete('galletas')
+listar_productos(productos)
